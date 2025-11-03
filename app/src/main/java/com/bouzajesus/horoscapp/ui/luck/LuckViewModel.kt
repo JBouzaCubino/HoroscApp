81a2f37336d0b77276a30ae32ac516e2f55861d8
Jesús <pilargn5@gmail.com>
2025-11-03 16:08:07 +0100

    LuckViewModel.kt implemented

--- app/src/main/java/com/bouzajesus/horoscapp/ui/luck/LuckViewModel.kt
+++ app/src/main/java/com/bouzajesus/horoscapp/ui/luck/LuckViewModel.kt
@@ -1,9 +1,26 @@
 package com.bouzajesus.horoscapp.ui.luck
 
 import androidx.lifecycle.ViewModel
+import com.bouzajesus.horoscapp.domain.usecases.GetRandomLuckyModelUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
+import jakarta.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 @HiltViewModel
-class LuckViewModel @Inject constructor(): ViewModel(){
+class LuckViewModel @Inject constructor(private val getRandomLuckyModelUseCase: GetRandomLuckyModelUseCase): ViewModel() {
+
+    private var _state = MutableStateFlow<LuckState>(LuckState.Error)
+    val state: StateFlow<LuckState> = _state
+
+    fun getRandomPrediction(){
+        val luckModel = getRandomLuckyModelUseCase.execute()
+
+        if(luckModel != null){
+            _state.value = LuckState.Success(luckModel)
+        }
+        else{
+            _state.value = LuckState.Error
+        }
+    }
 }
