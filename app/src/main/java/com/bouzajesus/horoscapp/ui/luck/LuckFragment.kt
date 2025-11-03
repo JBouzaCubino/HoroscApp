1c7ae121e33c6a18117e1105266ab3e27d1510ae
Jesús <pilargn5@gmail.com>
2025-11-03 16:10:23 +0100

    animations and viewmodel implemented

--- app/src/main/java/com/bouzajesus/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/bouzajesus/horoscapp/ui/luck/LuckFragment.kt
@@ -1,5 +1,6 @@
 package com.bouzajesus.horoscapp.ui.luck
 
+import android.animation.AnimatorInflater
 import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,9 +8,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.DecelerateInterpolator
+import android.widget.Toast
+import androidx.core.animation.doOnEnd
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.bouzajesus.horoscapp.R
 import com.bouzajesus.horoscapp.databinding.FragmentLuckBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import java.util.Random
 
 @AndroidEntryPoint
@@ -32,15 +39,74 @@ class LuckFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        luckViewModel.getRandomPrediction()
         initUI()
     }
 
     private fun initUI() {
         initListeners()
+
+        initUIState()
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            luckViewModel.state.collect { state ->
+                when(state){
+                    LuckState.Error -> errorState()
+                    is LuckState.Success -> successState(state.luckyModel.image, state.luckyModel.text)
+                }
+            }
+        }
+    }
+
+    private fun errorState(){
+        binding.tvLucky.text = getString(R.string.error)
+
+        Toast.makeText(this.context, "An error occurred", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun successState(image: Int, text: Int) {
+        binding.ivLuckyCard.setImageResource(image)
+        binding.tvLucky.text = getString(text)
     }
 
     private fun initListeners() {
         binding.ivCardRoulette.setOnClickListener { spinRoulette() }
+    }
+
+    private fun growCard(){
+        val animator = AnimatorInflater.loadAnimator(this.context, R.animator.grow_up)
+        animator.setTarget(binding.ivReverse)
+        animator.doOnEnd {
+            binding.ivReverse.isVisible = false
+            showPredictionView()
+        }
+        animator.start()
+    }
+
+    private fun slideCard(){
+        val animator = AnimatorInflater.loadAnimator(this.context, R.animator.slide_up)
+        animator.setTarget(binding.ivReverse)
+        binding.ivReverse.isVisible = true
+        animator.doOnEnd { growCard() }
+        animator.start()
+    }
+
+    private fun showPredictionView(){
+        val disappearAnimator = AnimatorInflater.loadAnimator(this.context, R.animator.disappear)
+        val appearAnimator = AnimatorInflater.loadAnimator(this.context, R.animator.appear)
+
+        disappearAnimator.setTarget(binding.preview)
+        appearAnimator.setTarget(binding.prediction)
+
+        disappearAnimator.doOnEnd {
+            binding.preview.isVisible = false
+            binding.prediction.isVisible = true
+        }
+
+        disappearAnimator.start()
+        appearAnimator.start()
     }
 
     private fun spinRoulette(){
@@ -49,6 +115,7 @@ class LuckFragment : Fragment() {
         val animator = ObjectAnimator.ofFloat(binding.ivCardRoulette, View.ROTATION, 0f, degrees.toFloat())
         animator.duration = 2000
         animator.interpolator = DecelerateInterpolator()
+        animator.doOnEnd { slideCard() }
         animator.start()
     }
 
